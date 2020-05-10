9571f63cbb4ba1a1c6807dc10cf49ed608b95910
jakdev047 <shuvo047niter@gmail.com>
2020-05-10 22:36:12 +0600

    to add node sass & reactstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {Container} from 'reactstrap';
+
+// style
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/sass/index.scss';
+
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h2>Hello react</h2>
+      </Container>
     </div>
   );
 }
