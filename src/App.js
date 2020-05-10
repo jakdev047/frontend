db667a157df0de0097d59acdbdbef7c6e84756ba
jakdev047 <shuvo047niter@gmail.com>
2020-05-10 23:40:04 +0600

    to create registration, signup, addnote, home, error page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React from 'react';
 
-import {Container} from 'reactstrap';
+// router
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 
 // style
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/sass/index.scss';
 
+// components
+import Header from './layout/Header';
+
+// pages
+import Home from './pages/Home';
+import Error from './pages/Error';
+import Registration from './pages/Registration';
+import Signup from './pages/Signup';
+import Addnotes from './pages/Addnotes';
 
 const App = () => {
   return (
-    <div className="App">
-      <Container>
-        <h2>Hello react</h2>
-      </Container>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/add-notes" component={Addnotes}/>
+          <Route exact path="/registration" component={Registration}/>
+          <Route exact path="/login" component={Signup}/>
+          <Route path="*" component={Error}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
